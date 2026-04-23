306bd5179c1c683234c66f3342a52679589e0cd7
Dumidu Himsara <dumiduhimsara19@gmail.com>
2026-04-23 23:08:55 +0530

    fix port for railway

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ mongoose.connect(mongourl).then(()=>{
     console.log(err);
 });
 
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
+
+
+
 app.use(express.json());
 
 // Register Route
